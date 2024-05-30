fb0ed0b22fe190f1e537dfe28c02c6ca163fe145
MAT-2 <mpcasey30@gmail.com>
2024-05-30 15:36:23 -0500

    Ran a test to see if API data is working. Used Goodfellas film API data as test via a console.log with a useEffect hook. Added JSX elements to see if the data would populate and it works.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import React from 'react';
 import {useEffect} from 'react';
+// import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=be441347';
+
+const movie1 = {
+    "Title": "Goodfellas",
+    "Year": "1990",
+    "imdbID": "tt0099685",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+};
 
 
-const API_URL = 'http://www.omdbapi.com?apikey=be441347'
 function App() {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -16,7 +26,39 @@ function App() {
   }, []);
 
   return (
-    <h1>App</h1> 
+    <div className="app">
+      <h1>Movie World</h1>
+
+      <div className="search">
+        <input 
+        placeholder="Search for Movies"
+        value="Goodfellas"
+        onChange={() => {}}
+        />
+
+        <img 
+        // src={SearchIcon} 
+        alt="search" 
+        onClick={() => {}}
+        />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img src={movie1.Poster != 'N/A' ? movie1.Poster : 'https://viaplaceholder.com/400'} alt={movie1.Title}/>
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div> 
   );
 }
 
