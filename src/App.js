8ffee59119f86c80897d1bdf04fc238675b4cdf5
MAT-2 <mpcasey30@gmail.com>
2024-05-30 19:32:58 -0500

    Updated App.js to grab more movies than just one. Added functionality for search bar to work.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import './App.css';
 import React from 'react';
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import MovieCard from './MovieCard'
 // import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=be441347';
 
-const movie1 = {
-    "Title": "Goodfellas",
-    "Year": "1990",
-    "imdbID": "tt0099685",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-};
+// const movie1 = {
+//     "Title": "Goodfellas",
+//     "Year": "1990",
+//     "imdbID": "tt0099685",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+// };
 
 
 function App() {
+
+  //created state here, using useState hook.
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('');
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
   useEffect(() => {
     searchMovies('Goodfellas');
@@ -30,23 +35,33 @@ function App() {
     <div className="app">
       <h1>Movie World</h1>
 
+{/* onChange event sets setSearchTerm setter function to target value. */}
       <div className="search">
         <input 
         placeholder="Search for Movies"
-        value="Goodfellas"
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         />
 
+{/* Added onclick event to use searchTerm state*/}
         <img 
         // src={SearchIcon} 
         alt="search" 
-        onClick={() => {}}
+        onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1}/>
-      </div>
+      {movies?.length > 0 
+      ? (
+        <div className="container">
+          {movies.map((movie) => (<MovieCard movie={movie}/>
+          ))}
+        </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies Found</h2>
+          </div>
+        )}
     </div> 
   );
 }
